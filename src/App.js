7062cc814bf668bc4a767daa3dbc736ef1db1186
Guillermo Maycotte <gmaycotte93@gmail.com>
2020-06-29 13:09:27 -0500

    init code ends

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 const App = () => {
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const res = await axios.get(
-        'https://my-json-server.typicode.com/gmayc/posts-code-challenge/posts',
-      );
-      console.log(res);
-      const test = await axios.get('http://localhost:3001');
-      console.log('fetchPosts -> test', test);
-    };
-    fetchPosts();
-    return () => {};
-  });
   return <div className='App'></div>;
 };
 
