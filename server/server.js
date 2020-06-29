d0f4894277ef740f6084fd167f77c693de0cb5af
Guillermo Maycotte <gmaycotte93@gmail.com>
2020-06-29 12:57:06 -0500

    sets cors logger and bodyparser on express server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(logger('dev'));
+app.use(cors());
+app.use(bodyParser.json({ limit: '5mb' }));
+app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get(
+        'https://my-json-server.typicode.com/gmayc/posts-code-challenge/posts',
+      );
+      console.log(res);
+      const test = await axios.get('http://localhost:3001');
+      console.log('fetchPosts -> test', test);
+    };
+    fetchPosts();
+    return () => {};
+  });
   return <div className='App'></div>;
-}
+};
 
 export default App;
